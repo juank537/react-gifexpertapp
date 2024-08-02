061523c76ff1e21a8a63748000b3dafdc16fd8e8
Juan Carlos Aliaga <juank537@gmail.com>
2024-08-02 16:27:14 -0400

    First commit

---
+++ src/components/GifGrid.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { GifGridItem }  from './GifGridItem';
+import { useFetchGifs } from '../hooks/useFetchGifs';
+
+
+const GifGrid = ({ category }) => {
+
+    const { data:images, loading } = useFetchGifs( category );
+
+    return (
+        <>
+            <h3 className='animate__animated animate__pulse'>{ category }</h3>
+            
+            { loading && <p className='animate__animated animate__flash'>Loading</p> }
+
+            <div className='card-grid'>
+                {
+                    images.map( img => (
+                        <GifGridItem  
+                            key={ img.id }
+                            { ...img }
+                        />
+                    ))
+                }
+            </div>
+        </>
+    )
+}
+
+export default GifGrid
